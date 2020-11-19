7ca1be092af123b06b941a62c111a64453e2867a
handsomemolly <ryan.kendig@gmail.com>
2020-11-19 08:29:27 -0800

    commit tree 53d32737ed6f2ba411d6c993a97d2b0d2eb9b530

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,50 @@
 # code here!
+
+require 'pry'
+class School
+
+attr_accessor :name, :roster
+# attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    # def name=(name)
+    #     @name = name
+    # end
+
+    # def name
+    #     @name
+    # end
+
+    # def roster=(roster)
+    #     @roster = {}
+    # end
+
+    # def roster
+    #     @roster
+    # end
+
+    def add_student(student, grade)
+        if @roster[grade]
+            @roster[grade].push student
+            binding.pry
+        else
+            @roster[grade] = [student]
+            
+        end
+    end
+
+    def grade(level)
+        @roster[level]
+    end
+
+    def sort
+       
+    end
+
+
+
+end
