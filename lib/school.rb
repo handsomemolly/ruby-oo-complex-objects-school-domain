f39265a57b21a022cc0ff2628fe33d0964c50cf2
handsomemolly <ryan.kendig@gmail.com>
2020-11-22 12:32:34 -0800

    finally sorted roster

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+
+attr_accessor :name, :roster
+# attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if self.roster[grade]
+            self.roster[grade].push student
+            
+        else
+            self.roster[grade] = [student]
+            
+        end
+    end
+
+    def grade(level)
+        self.roster[level]
+    end
+
+    def sort 
+        sorted_roster_hash = {}
+        roster.each {|grade, s| sorted_roster_hash[grade]= s.sort}
+        sorted_roster_hash
+    end
+
+end
